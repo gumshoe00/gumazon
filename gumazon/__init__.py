bb0ba72a1fa9dc182d768d20dcefc7fb1140b135
gumshoe00 <65322760+gumshoe00@users.noreply.github.com>
2022-01-27 16:56:45 -0700

    Added Gumazon Source

---
+++ gumazon/__init__.py
@@ -0,0 +1,10 @@
+__version__ = '0.1.0'
+
+# general:
+__title__ = "Gumazon Ads Cross-Locals Visibility."
+__author__ = 'Gumshoe Media Inc.'
+__email__ = 'gumshoe.media.inc@gmail.com'
+
+# license:
+__license_setup_title__ = 'GNU General Public License v3'
+__license_setup_classifier__ = 'GNU General Public License v3 (GPLv3)'
